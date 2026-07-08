d85d9aeb6554e2c54f253edd7a24f4d661fb0cba
Miguel Corredera <miguelcoace@outlook.es>
2026-07-08 12:51:17 +0200

    Header Full Responsive 1.0

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import "../styles/base/_animations.scss";
+import "../styles/base/_typography.scss";
+import "../styles/components/_buttons.scss";
+import "../styles/layout/_header.scss";
+function Header() {
+  return (
+    <>
+      <header className="header">
+        <div className="hero">
+          <h1>Tu tienda hippie del Rastro de Madrid</h1>
+          <div className="grid">
+            <button className="btn">Ver Categorias</button>
+            <button className="btn historia">Nuestra Historia</button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./components/App.tsx";
+import Header from "./components/Header.tsx";
+import Separator from "./components/Separator.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Header />
+    <Separator />
     <App />
   </StrictMode>,
 );

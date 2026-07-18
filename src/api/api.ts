d7bfa03c31973e70ee128710f9947eef7aac361d
Miguel Corredera <miguelcoace@outlook.es>
2026-07-18 12:25:29 +0200

    Api conexion 0.2

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8000";
+const API_URL = "http://192.168.99.190:8000";
 
 export async function apiFetch(endpoint: string, options: RequestInit = {}) {
   return fetch(`${API_URL}${endpoint}`, {
